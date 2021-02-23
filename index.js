0ab7d3c1aa9b877514e66fb2a1ce7213771b3add
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-02-23 10:52:37 +0530

    typing effect added

--- index.js
+++ index.js
@@ -16,8 +16,24 @@ function type(){
         char_idx++
         setTimeout(type,typing_delay)
     }else{
-
+        setTimeout(erase,new_text_delay)
     }
 }   
 
-type()
+function erase(){
+    if(char_idx > 0){
+        typed_text.textContent = text_array[array_idx].substring(0,char_idx-1)
+        char_idx-=1
+        setTimeout(erase,erase_delay)
+    }else{
+        array_idx+=1
+        if(array_idx >= text_array.length){
+            array_idx=0
+        }
+        setTimeout(type,typing_delay + 1100)
+    }
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+    setTimeout(type,new_text_delay + 250)
+})
