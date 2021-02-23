a0317ac5c297e05e87bcd256134cb50af93e8c4e
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-02-23 11:20:10 +0530

    typing effect added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const typed_text = document.querySelector('.typed-text')
+const cursor = document.querySelector('.cursor')
 
-const text_array = ['a competitve programmer' , 'and a web developer']
+const text_array = ['a Competitve Programmer and ' , 'a Web Developer.']
 
 const typing_delay = 200
 const erase_delay = 100
@@ -12,20 +13,28 @@ let char_idx = 0
 
 function type(){
     if(char_idx < text_array[array_idx].length){
+        if(!cursor.classList.contains("typing")){
+            cursor.classList.add("typing")
+        }
         typed_text.textContent += text_array[array_idx].charAt(char_idx)
         char_idx++
         setTimeout(type,typing_delay)
     }else{
+        cursor.classList.remove("typing")
         setTimeout(erase,new_text_delay)
     }
 }   
 
 function erase(){
     if(char_idx > 0){
+        if(!cursor.classList.contains("typing")){
+            cursor.classList.add("typing")
+        }
         typed_text.textContent = text_array[array_idx].substring(0,char_idx-1)
         char_idx-=1
         setTimeout(erase,erase_delay)
     }else{
+        cursor.classList.remove("typing")
         array_idx+=1
         if(array_idx >= text_array.length){
             array_idx=0
